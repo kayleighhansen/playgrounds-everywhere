47322260ccaca443b730b09fc36fa8483689c705
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-09-23 14:27:53 -0600

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FollowUpComponent } from './follow-up/follow-up.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    FollowUpComponent,
+    ProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
