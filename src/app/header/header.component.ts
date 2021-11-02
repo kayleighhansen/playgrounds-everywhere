b8f222fb92792be96e6a2eae85e5aed917e80b14
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-11-01 22:46:01 -0600

    forms completed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,23 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['../app.component.css']
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-
-  myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav";
-    }
   }
 
 }
