b08fa7b31e681b1d2ae55a8e7e778d756725fa7d
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-11-12 22:27:21 -0700

    ...

--- src/app/organizations/organization-details/organization-details.component.ts
+++ src/app/organizations/organization-details/organization-details.component.ts
@@ -3,6 +3,8 @@ import { Organization } from '../organization.model';
 import { OrganizationService } from '../organization.service';
 import { Router } from '@angular/router';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-organization-details',
@@ -10,19 +12,47 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['../../app.component.css']
 })
 export class OrganizationDetailsComponent implements OnInit {
-  @Input() organization: Organization;
-  id: string;
+  public organizations: Organization[] = [];
+  public organization: Organization;
+  id: string; 
+  public organizationsArray: Organization[] = [];
+  fetchOrganizationsSubscription: Subscription;
+
+  httpUrl = ""
+  error: string;
 
   constructor(private organizationService: OrganizationService,
-              private router: Router,
-              private route: ActivatedRoute) { }
+              private router: Router, 
+              private route: ActivatedRoute,
+              private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.route.params
-    .subscribe((params: Params) => {
-      this.id = params['organization.id'];
-      //this.organization = this.organizationService.getOrganization(this.id);
-    });  
+ 
+    const singleOrganization = this.organizationService.fetchOrganizations();
+
+    this.id = this.route.url.toString().replace('http://localhost:4200/organizations/', '');
+
+    this.fetchOrganizationsSubscription = this.organizationService.fetchOrganizationsEvent.subscribe((result)=> {
+      if(this.id = result[0].id) {
+        this.organization = result[0];
+      }
+        console.log(this.organization);
+
+    }, error => {
+      this.error = error.message;
+    });
+
   }
 
+  onDelete(id) {
+    console.log(id);
+    this.organizationService.deleteOrganization(id).subscribe(() => { 
+      this.organizations = [id];
+      this.router.navigate(['/organizations']);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.fetchOrganizationsSubscription.unsubscribe();
+  }
 }
--- src/app/organizations/organization-list/organization-list.component.ts
+++ src/app/organizations/organization-list/organization-list.component.ts
@@ -3,31 +3,41 @@ import { Organization } from '../organization.model';
 import { Subscription } from 'rxjs';
 import { OrganizationService } from '../organization.service';
 
-
 @Component({
   selector: 'app-organization-list',
   templateUrl: './organization-list.component.html',
   styleUrls: ['../../app.component.css']
 })
-export class OrganizationListComponent implements OnInit {
-  @Output() organizationWasSelected = new EventEmitter<Organization>();
+export class OrganizationListComponent implements OnInit, OnDestroy {
 
-  public organizations: Organization[];
+  public organizations: Organization[] = [];
+  public organizationsArray: Organization[] = [];
   fetchOrganizationsSubscription: Subscription;
 
-  constructor(private organizationService: OrganizationService) { }
+  error: string;
+  isFetching: boolean = false;
+  isEmpty: boolean = false;
 
-  OnFetchOrganizations() {
-    this.organizationService.getOrganizations();
-  }
+  constructor(private organizationService: OrganizationService) { }
 
   ngOnInit(): void {
     const list = this.organizationService.fetchOrganizations();
 
-    this.fetchOrganizationsSubscription = this.organizationService.fetchOrganizationsEvent.subscribe((result => {
+    this.isFetching = true;
+
+    this.fetchOrganizationsSubscription = this.organizationService.fetchOrganizationsEvent.subscribe((result) => {
+      this.isFetching = false;
+
       this.organizations = result;
       console.log(this.organizations);
-    }))
+
+      if(this.organizations.length < 1) {
+        this.isEmpty= true;
+      }
+
+    }, error => {
+      this.error = error.message;
+    });
   }
 
   ngOnDestroy(): void {
