e0bc99d77a20930a49c653f97bb2e33ccfdb02c5
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-11-24 23:36:05 -0700

    ...

--- src/app/projects/project-edit/project-edit.component.ts
+++ src/app/projects/project-edit/project-edit.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Contact } from 'src/app/contacts/contact.model';
+import { ContactService } from '../../contacts/contact.service';
+import { Organization } from 'src/app/organizations/organization.model';
+import { OrganizationService } from '../../organizations/organization.service';
+import { NgForm } from '@angular/forms';
+import { Project } from '../project.model';
+import { ProjectService } from '../project.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-project-edit',
@@ -7,9 +16,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectEditComponent implements OnInit {
 
-  constructor() { }
+  fetchContactsSubscription: Subscription;
+  fetchOrganizationsSubscription: Subscription;
+  contact: Contact;
+  project: Project;
+  organization: Organization;
+  contacts: Contact[] = [];
+  organizations: Organization[] = [];
+  
+  constructor(private projectService: ProjectService,
+              private organizationService: OrganizationService,
+              private contactService: ContactService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
+
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newProject = new Project(
+      "0", 
+      value.name, 
+      value.organizationId, 
+      value.contactId, 
+      value.date, 
+      value.country, 
+      value.city, 
+      value.equipment, 
+      value.equipmentAmount,
+      value.donation, 
+      value.donationsAmount,
+      value.price, 
+      value.details,
+      value.results
+    );
+
+    console.log(newProject);
+    this.projectService.addProject(newProject);
+
+    //this.router.navigate(['/projects']);
+  }
+
 
 }
